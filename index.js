b6ba6201b019c0947d5e18044db9313fc4b070e7
aubford <musicaubrey@gmail.com>
2021-02-14 17:14:32 -1000

    refactored and working for multiple tickers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer")
 const { webSocketDebuggerUrl } = require("./ws.json")
+const _ = require("lodash")
 const { newBrowserPage } = require("./util")
 const mergeImg = require("merge-img")
 
@@ -11,38 +12,50 @@ const connection = {
   }
 }
 
+const tickers = ["C", "SEDG", "T"]
+
 puppeteer.connect(connection).then(async browser => {
   const newPage = url => newBrowserPage(browser, url)
 
-  const fetchData = async (url, xPathArr, screenShotArr) => {
-    const page = await newPage(url)
-    await page.waitForXPath(xPathArr[0])
+  const tickerData = {}
+  for (let ticker of tickers) {
+    let pics = []
+    const fetchData = async (url, xPathArr, screenShotArr) => {
+      const page = await newPage(url)
+      await page.waitForXPath(xPathArr[0])
 
-    const screenShots = await Promise.all(screenShotArr.map(clip => page.screenshot({ clip })))
-    const mergedJimpObj = await mergeImg(screenShots)
-    await mergedJimpObj.write("./zebratime.png")
+      const values = await Promise.all(xPathArr.map(page.getTextByX))
 
-    const values = await Promise.all(xPathArr.map(page.getTextByX))
-    return values
+      const screenShots = await Promise.all(screenShotArr.map(clip => page.screenshot({ clip })))
+      pics = pics.concat(screenShots)
+      return values
+    }
+
+    const fordData = await fetchData(
+      `https://research.ameritrade.com/grid/wwws/research/reports/viewreport?id=130&documenttag=${ticker}&c_name=invest_VENDOR`,
+      [
+        `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[36]`,
+        `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[46]`,
+        `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[53]`,
+        `//span[contains(text(),' performance is ')]/following-sibling::span`
+      ],
+      [
+        { x: 330, y: 175, width: 250, height: 100 },
+        { x: 330, y: 1000, width: 250, height: 250 }
+      ]
+    )
+
+    // results
+    const mergedJimpObj = await mergeImg(pics)
+    await mergedJimpObj.write(`/Users/aubreyford/Desktop/Stock-Scrapbook/${ticker}.png`)
+    tickerData[ticker] = {
+      fordEarningsStrength: fordData[0],
+      fordRelativeValuation: fordData[1],
+      fordPriceMovement: fordData[2],
+      fordIndustryStrength: fordData[3]
+    }
   }
 
-  const TICKER = "SEDG"
-
-  const fordData = await fetchData(
-    `https://research.ameritrade.com/grid/wwws/research/reports/viewreport?id=130&documenttag=${TICKER}&c_name=invest_VENDOR`,
-    [
-      `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[36]`,
-      `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[46]`,
-      `/html/body/div[1]/div[2]/div[4]/div/div[1]/div[2]/span[53]`,
-      `//span[contains(text(),' performance is ')]/following-sibling::span`
-    ],
-    [
-      { x: 330, y: 175, width: 250, height: 100 },
-      { x: 330, y: 1000, width: 250, height: 250 }
-    ]
-  )
-
-  console.log(JSON.stringify(fordData))
-
+  console.log(JSON.stringify(tickerData))
   process.exit(0)
 })
