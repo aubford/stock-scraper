13a9960c3be9b82c23f783155bb5d9377683703b
Aubrey <MusicAubrey@gmail.com>
2021-10-06 14:52:34 -0400

    gitignore fix

---
+++ test/scratch.js
@@ -0,0 +1,15 @@
+//const _ = require("lodash")
+//const yahooData = require("./data/cData.json")
+//const moment = require("moment")
+//const wsjData = require("./data/wsjC.json")
+//
+//const buildCompanyData = require("../src/buildCompanyData")
+//const { getStatementCharts } = require("../src/buildCompanyDataUtil")
+//
+//const {
+//  quoteSummary: { result },
+//} = yahooData
+const _ = require("lodash")
+const arr = [5, 6, 7]
+
+arr[arr.length - 2] /*?*/
