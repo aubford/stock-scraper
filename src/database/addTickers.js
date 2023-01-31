64614348c77c415c965c37517c1ce21ea211dcf2
Aubrey Ford <aubrey.ford@nutrien.com>
2023-01-30 16:03:16 -0800

    fix addTickers

--- src/database/addTickers.js
+++ src/database/addTickers.js
@@ -1,10 +1,12 @@
-const { scrapbookWriteOut, promptForTickers } = require("../util")
+const { promptForTickers, readFile, writeFile } = require("../util")
 
 // want new tickers to update first when running update.js script
 const randomOldDate = new Date(2000, 7, 24)
+const stockFile = readFile(STOCK_DATA_LOCATION)
+const existingTickers = Object.keys(stockFile)
 
 promptForTickers().then(promptRes => {
-  const tickers = promptRes.split(/[^A-Z]/).filter(a => a)
+  const tickers = promptRes.split(/[^A-Z]/).filter(a => a && !existingTickers.includes(a))
   const toAdd = tickers.reduce(
     (acc, ticker) => ({
       [ticker]: { sector: "NEW_STOCKS", scrapeDataUpdatedAt: randomOldDate, ticker },
@@ -12,6 +14,6 @@ promptForTickers().then(promptRes => {
     }),
     {}
   )
-  scrapbookWriteOut(toAdd)
+  writeFile(STOCK_DATA_LOCATION, { ...stockFile, ...toAdd })
   process.exit(0)
 })
