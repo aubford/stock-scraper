69bac3e9b73d5d73c79662546b8a20502878ca5c
Aubrey Ford <aubrey.ford@nutrien.com>
2023-02-14 14:19:13 -0500

    add vooAddTickers

---
+++ src/database/vooAddTickers.js
@@ -0,0 +1,19 @@
+const { promptForTickers, readFile, writeFile } = require("../util")
+
+// want new tickers to update first when running update.js script
+const randomOldDate = new Date(2000, 7, 24)
+const stockFile = readFile(VOO_LOCATION)
+const existingTickers = Object.keys(stockFile)
+
+promptForTickers().then(promptRes => {
+  const tickers = promptRes.split(/[^A-Z]/).filter(a => a && !existingTickers.includes(a))
+  const toAdd = tickers.reduce(
+    (acc, ticker) => ({
+      [ticker]: { sector: "NEW_STOCKS", scrapeDataUpdatedAt: randomOldDate, ticker },
+      ...acc,
+    }),
+    {}
+  )
+  writeFile(VOO_LOCATION, { ...stockFile, ...toAdd })
+  process.exit(0)
+})
