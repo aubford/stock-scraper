131156a461f0b465c98fac3425f11f1d81a44c44
Aubrey Ford <aubrey.ford@nutrien.com>
2023-03-05 22:13:05 -0800

    fix moodys login

--- src/util.js
+++ src/util.js
@@ -116,7 +116,7 @@ const promptLogin = newPage => {
   const pages = [
     "https://invest.ameritrade.com/grid/p/site#r=jPage/https://research.ameritrade.com/grid/wwws/research/stocks/analystreports?symbol=USB&c_name=invest_VENDOR",
     "https://oltx.fidelity.com/ftgw/fbc/oftop/portfolio#summary",
-    "https://www.moodys.com/credit-ratings/ATT-Inc-credit-rating-702550",
+    "https://www.moodys.com",
     "https://olui2.fs.ml.com/TFPHoldings/HoldingsByAccount.aspx?as_cd=1.4.2147483647.-1",
   ].map(url => newPage(url, { waitUntil: "domcontentloaded" }))
 
