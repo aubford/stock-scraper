0d3326085d175162750a40bd7da583d435985846
Aubrey Ford <aubrey.ford@nutrien.com>
2023-07-18 10:13:08 -0600

    fix yahoo and nc

--- src/api/newConstructs.js
+++ src/api/newConstructs.js
@@ -10,20 +10,25 @@ const { handleFetch } = require("./util/www")
  * @returns {Promise<{}|{ncRoic:*, ncPB:*, ncRating:*, ncFCF:*, ncGap:*, ncEps:*}>}
  */
 const fetchData = async (ticker, browser) => {
-  const [ncPeriodEndDateStr, [ncRating, ncRoic, ncFCF, ncEps, ncGap, ncPB] = [], isSuspended] =
-    await fetchPdfData({
-      ticker,
-      browser,
-      analystName: NEW_CONSTRUCTS,
-      url: `https://research.ameritrade.com/grid/wwws/research/reports/viewreport?id=2942&documenttag=${ticker}&c_name=invest_VENDOR`,
-      xPathArr: [
-        selfTextContains("for period ending"),
-        `//span[text()="1 - Very Attractive" or text()="2 - Attractive" or text()="3 - Neutral"  or text()="4 - Unattractive" or text()="5 - Very Unattractive"]`,
-        selfTextContains("Suspended"),
-      ],
-      waitForPostScroll: `//span[contains(text(),"Price-to-EBV Ratio is")]`,
-      timeout: NEW_CONSTRUCTS_TIMEOUT,
-    })
+  const res = await fetchPdfData({
+    ticker,
+    browser,
+    analystName: NEW_CONSTRUCTS,
+    url: `https://research.ameritrade.com/grid/wwws/research/reports/viewreport?id=2942&documenttag=${ticker}&c_name=invest_VENDOR`,
+    xPathArr: [
+      selfTextContains("for period ending"),
+      `//span[text()="1 - Very Attractive" or text()="2 - Attractive" or text()="3 - Neutral"  or text()="4 - Unattractive" or text()="5 - Very Unattractive"]`,
+      selfTextContains("Suspended"),
+    ],
+    waitForPostScroll: `//span[contains(text(),"Price-to-EBV Ratio is")]`,
+    timeout: NEW_CONSTRUCTS_TIMEOUT,
+  })
+
+  const [
+    ncPeriodEndDateStr,
+    [, , , , , ncRating, ncRoic, ncFCF, ncEps, ncGap, ncPB] = [],
+    isSuspended,
+  ] = res
 
   const periodEndDate = ncPeriodEndDateStr ? last(ncPeriodEndDateStr.split(" ")) : ""
 
