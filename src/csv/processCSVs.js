e13c188d8ba66f537158423059d99205a89812bb
Aubrey Ford <aubrey.ford@nutrien.com>
2023-08-02 10:49:24 -0600

    add open

--- src/csv/processCSVs.js
+++ src/csv/processCSVs.js
@@ -1,4 +1,6 @@
 const { groupBy, mapValues } = require("lodash")
+const open = require("open")
+
 const {
   renameCSVs,
   unrealizedPath,
@@ -6,13 +8,16 @@ const {
   getUnrealizedCostBasis,
   getUnrealizedShares,
 } = require("./csv-util")
-const { writeToExistingTickers } = require("../util")
+const { writeToExistingTickers, promptUser } = require("../util")
 
 renameCSVs()
+
+open("https://olui2.fs.ml.com/TFPDownloads/TFPDownloads.aspx")
 
 const SYMBOL_HEADER = "Symbol"
 
 const main = async () => {
+  await promptUser("Download CSV and then press Enter")
   const unrealizedTaxLotsCsv = await parseCSV(unrealizedPath)
   const unrealizedTaxLots = groupBy(unrealizedTaxLotsCsv, SYMBOL_HEADER)
 
