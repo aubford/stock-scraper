09749d24755f04ea144c1535b8a4907d2dd26927
Aubrey Ford <aubrey.ford@nutrien.com>
2023-08-03 16:23:09 -0600

    no argless start script

--- src/apps/start.js
+++ src/apps/start.js
@@ -1,14 +1,12 @@
 const puppeteer = require("puppeteer-core")
-const { exit, getStockTickers } = require("../util")
+const { exit } = require("../util")
 const scrapeDataForTickers = require("../scrapeDataForTickers")
 const { beginAndLogin } = require("../puppeteer")
 
 puppeteer.connect(CONNECTION).then(async browser => {
   const promptResponse = await beginAndLogin(browser, "Tickers: ")
 
-  const tickers = promptResponse
-    ? promptResponse.split(/[^A-Z]/).filter(a => a)
-    : getStockTickers()
+  const tickers = promptResponse.split(/[^A-Z]/).filter(a => a)
 
   await scrapeDataForTickers(tickers, browser, SHOULD_MERGE)
 
