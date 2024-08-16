9bda9e4985d8758962769e5fcd29f4e01e95be29
Aubrey <MusicAubrey@gmail.com>
2024-08-16 12:52:58 -0700

    start

--- src/apps/start.js
+++ src/apps/start.js
@@ -6,6 +6,12 @@ connectAndRunApp(async browser => {
   const promptResponse = await beginAndLogin(browser, "Tickers: ")
 
   const tickers = promptResponse.split(/[^A-Z]/).filter(a => a)
+  const fs = require("fs")
+
+  if (!fs.existsSync(STOCK_DATA_STAGING)) {
+    fs.copyFileSync(STOCK_DATA_LOCATION, STOCK_DATA_STAGING)
+  } 
+    
 
   await scrapeDataForTickers(tickers, browser)
 
