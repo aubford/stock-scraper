ce5fc42de4f0eb927e448b261d539f97d317bcee
Aubrey <MusicAubrey@gmail.com>
2024-10-22 19:14:36 -0400

    remove redundant globalEnv code in index.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,5 @@
-const { promptUser } = require("./util")
 require("../globalEnv")
-
-try {
-  const { webSocketDebuggerUrl } = require("../ws.json")
-  global.CONNECTION = {
-    browserWSEndpoint: webSocketDebuggerUrl,
-    defaultViewport: {
-      width: 1400,
-      height: 1800,
-    },
-  }
-} catch (err) {
-  console.log("skipped ws connection")
-}
-
-// Error.stackTraceLimit = 1
-
-const timeoutCoeff = 2
-
-global.PAUSE_MS = 1000 * timeoutCoeff
-global.DEFAULT_NAVIGATION_TIMEOUT = 60 * 1000 * timeoutCoeff
-global.XPATH_TIMEOUT = 30 * 1000 * timeoutCoeff
-
-global.WSJ_TIMEOUT = 10 * 1000 * timeoutCoeff
-global.FIDELITY_ANALYST_TIMEOUT = 25 * 1000 * timeoutCoeff
-global.MOODYS_TIMEOUT = 20 * 1000 * timeoutCoeff
-global.CFRA_TIMEOUT = 20 * 1000 * timeoutCoeff
-global.BOA_TIMEOUT = 10 * 1000 * timeoutCoeff
-global.ARGUS_ANALYST_TIMEOUT = 10 * 1000 * timeoutCoeff
-global.MORNINGSTAR_TIMEOUT = 20 * 1000 * timeoutCoeff
-
-global.SCRAPBOOK_LOCATION = process.env.STOCK_SCRAPBOOK_LOCATION
-global.STOCK_DATA_LOCATION = `${SCRAPBOOK_LOCATION}/stockData.json`
-global.STOCK_DATA_STAGING = `${__dirname}/../stockDataStaging.json`
-global.VOO_DATA_STAGING = `${__dirname}/../vooDataStaging.json`
-global.VOO_LOCATION = `${SCRAPBOOK_LOCATION}/vooData.json`
-global.META_LOCATION = `${SCRAPBOOK_LOCATION}/stockDataMeta.json`
-
-Promise.stagger = async (asyncFunc, paramArr, ms) => {
-  const staggered = paramArr.map(async (params, idx) => {
-    await new Promise(resolve => setTimeout(resolve, idx * ms))
-    const normalized = [].concat(params)
-    return asyncFunc(...normalized)
-  })
-  return await Promise.all(staggered)
-}
-
+const { promptUser } = require("./util")
 const fs = require("fs")
 const path = require("path")
 
