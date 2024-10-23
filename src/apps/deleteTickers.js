ebe1e8a31e2c3bd81717f2352dae13b3df8a0596
Aubrey <MusicAubrey@gmail.com>
2024-10-23 13:47:31 -0400

    add deleteTickers

---
+++ src/apps/deleteTickers.js
@@ -0,0 +1,17 @@
+const { writeJsonFile, promptForTickers, getStockDataFile } = require("../util")
+const showTickers = require("./showTickers")
+
+module.exports = async () => {
+  const tickers = await promptForTickers()
+  const stockDataFile = getStockDataFile()
+
+  for (const ticker in stockDataFile) {
+    if (tickers.includes(ticker)) {
+      delete stockDataFile[ticker]
+    }
+  }
+
+  writeJsonFile(STOCK_DATA_LOCATION, stockDataFile)
+  console.log("New Tickers: ")
+  showTickers()
+}
