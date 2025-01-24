85fc92412fe5288d20f8c4be489f3096a361cab3
Aubrey <MusicAubrey@gmail.com>
2025-01-24 10:49:42 -0800

    abort if no staging file

--- src/apps/commit.js
+++ src/apps/commit.js
@@ -1,7 +1,14 @@
 const { writeOut, deleteFile, readJsonFile, promptForYes } = require("../util")
+const fs = require("fs")
 
 const commit = async (isVoo, merge) => {
   const stagingFileLocation = isVoo ? VOO_DATA_STAGING : STOCK_DATA_STAGING
+
+  if (!fs.existsSync(stagingFileLocation)) {
+    console.log(`No staging file found at ${stagingFileLocation}. Skipping.`)
+    return
+  }
+
   const newDataToWrite = readJsonFile(stagingFileLocation)
   const fileLocation = isVoo ? VOO_LOCATION : STOCK_DATA_LOCATION
 
