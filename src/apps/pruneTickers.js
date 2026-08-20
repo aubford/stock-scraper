198fd483eceef5dd82e3667731fe988508a61b13
Aubrey <MusicAubrey@gmail.com>
2026-08-20 15:45:20 -0700

    prunetickers reporting

--- src/apps/pruneTickers.js
+++ src/apps/pruneTickers.js
@@ -5,13 +5,16 @@ module.exports = async () => {
   const tickers = await promptForTickers()
   const stockDataFile = getStockDataFile()
 
+  const prunedTickers = []
   for (const ticker in stockDataFile) {
     if (!tickers.includes(ticker)) {
       delete stockDataFile[ticker]
+      prunedTickers.push(ticker)
     }
   }
 
   writeJsonFile(STOCK_DATA_LOCATION, stockDataFile)
+  console.log("Pruned tickers: " + prunedTickers.sort())
   console.log("New Tickers: ")
   showTickers()
 }
